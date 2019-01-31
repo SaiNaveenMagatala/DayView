38548dbdad8240d4ec5671294c3c9625247b4a9c
NaveenMagatala <naveenmagatala@gmail.com>
2019-01-31 11:00:54 -0500

    initial setup

---
+++ Example/Pods/Target Support Files/DayView/DayView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_DayView : NSObject
+@end
+@implementation PodsDummy_DayView
+@end
---
+++ Example/Pods/Target Support Files/Pods-DayView_Tests/Pods-DayView_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DayView_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_DayView_Tests
+@end
